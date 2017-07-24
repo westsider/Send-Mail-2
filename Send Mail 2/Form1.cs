d1ff41db7895cad0d7ac476f35a417a0fbaacfd5
Warren Hansen <whansen1@mac.com>
2017-07-24 02:29:33 -0700

    task: Finished implementation

--- Send Mail 2/Form1.cs
+++ Send Mail 2/Form1.cs
@@ -14,8 +14,8 @@ namespace Send_Mail_2
 {
     public partial class Form1 : Form
     {
-        public string toEmailAddress_1 = "whansen1@mac.com";
-        public string toEmailAddress_2 = "3103824522@tmomail.net";
+        public string toEmailAddress = "whansen1@mac.com";
+        public string toSmsAddress = "3103824522@tmomail.net";
         public string emailSubject = "Test from VS";
         public string emailMessage = "Message from VS";
         public string fromEmailAddress = "trader1@tradestrat.net";
@@ -27,16 +27,27 @@ namespace Send_Mail_2
         }
         public void sendTheMail()
         {
-            SmtpClient sc = new SmtpClient();
-            sc.Port = 25;
-            sc.Host = "smtp.stackmail.com";
-            sc.EnableSsl = false;
-            sc.Credentials = new NetworkCredential(fromEmailAddress, fromEmailPass);
+            SmtpClient smtp = new SmtpClient(fromEmailAddress);
+            smtp.Port = 25;
+            smtp.Host = "smtp.stackmail.com";
+            smtp.EnableSsl = false;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(fromEmailAddress, fromEmailPass);
             MailMessage mail = new MailMessage();
             mail.From = new MailAddress(fromEmailAddress, "VPN Trade Stats");
-            mail.To.Add(toEmailAddress_1);
-            mail.Subject = emailSubject; mail.IsBodyHtml = true; mail.Body = emailMessage;
-            sc.Send(mail);
+            mail.To.Add(toEmailAddress);
+            mail.To.Add(toSmsAddress);
+            mail.Subject = emailSubject; mail.IsBodyHtml = false; mail.Body = emailMessage;
+            try
+            {
+                smtp.Send(mail);
+                MessageBox.Show("Sent Successfully", "Message",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error ", ex);
+            }
+            
         }
 
         private void sendMailBttn_Click(object sender, EventArgs e)
