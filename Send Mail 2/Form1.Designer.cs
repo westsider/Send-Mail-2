7889721a9e8481c8c1188737b548351529192381
Warren Hansen <whansen1@mac.com>
2017-07-23 22:16:23 -0700

    task: initial commit

--- Send Mail 2/Form1.Designer.cs
+++ Send Mail 2/Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sendMailBttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sendMailBttn
+            // 
+            this.sendMailBttn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.sendMailBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendMailBttn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sendMailBttn.Location = new System.Drawing.Point(55, 91);
+            this.sendMailBttn.Name = "sendMailBttn";
+            this.sendMailBttn.Size = new System.Drawing.Size(170, 43);
+            this.sendMailBttn.TabIndex = 0;
+            this.sendMailBttn.Text = "send e-mail";
+            this.sendMailBttn.UseVisualStyleBackColor = false;
+            this.sendMailBttn.Click += new System.EventHandler(this.sendMailBttn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.sendMailBttn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button sendMailBttn;
     }
 }
 
--- Send Mail 2/Form1.cs
+++ Send Mail 2/Form1.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
 
 namespace Send_Mail_2
 {
     public partial class Form1 : Form
     {
+        public string toEmailAddress_1 = "whansen1@mac.com";
+        public string toEmailAddress_2 = "3103824522@tmomail.net";
+        public string emailSubject = "Test from VS";
+        public string emailMessage = "Message from VS";
+        public string fromEmailAddress = "trader1@tradestrat.net";
+        public string fromEmailPass = "Si062fcaa";
+
         public Form1()
         {
             InitializeComponent();
         }
+        public void sendTheMail()
+        {
+            SmtpClient sc = new SmtpClient();
+            sc.Port = 25;
+            sc.Host = "smtp.stackmail.com";
+            sc.EnableSsl = false;
+            sc.Credentials = new NetworkCredential(fromEmailAddress, fromEmailPass);
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(fromEmailAddress, "VPN Trade Stats");
+            mail.To.Add(toEmailAddress_1);
+            mail.Subject = emailSubject; mail.IsBodyHtml = true; mail.Body = emailMessage;
+            sc.Send(mail);
+        }
+
+        private void sendMailBttn_Click(object sender, EventArgs e)
+        {
+            sendTheMail();
+            Console.WriteLine("Clicked Send Mail");
+
+            // TODO:- mail sent + completion handler -> sent or error
+            // TODO - Add 2nd email address
+        }
+
+   
     }
 }
+       
